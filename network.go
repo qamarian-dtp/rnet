3f50a963227a15f496073d049ff41d8aa087ee92
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-07-31 05:00:18 +0100

    Progress 8

--- network.go
+++ network.go
@@ -1,43 +1,53 @@
 package rnet
 
 import (
+	"errors"
+	"gopkg.in/qamarian-lib/str.v1"
 	"sync"
 )
 
-func New () (*Network) {
-	// id
-	net := Network {id, false, false, sync.Map {},
-
-		struct {
-			locked int8
-			addr map[string][]string
-		},
-
-		struct {
-			locked int8
-			addr map[string]bool
-		},
+func New () (*Network, error) {
+	id, errX := str.UniquePredsafeStr (32)
+	if errX != nil {
+		errMssg := fmt.Sprintf ("Unable to generate ID for network. [%s]", errX.Error ())
+		return nil, errors.New (errMssg)
 	}
-	return net
+	net := Network {}
+	return &net, nil
 }
 
 type Network struct {
 	id string
-	freezed bool
-	locked bool
-
+	locked struct
+		locker sync.RWMutex
+		locked bool
+	}
+	freezed struct {
+		locker sync.RWMutex
+		freezed bool
+	}
 	allocations sync.Map // KEY: net-addr; VAL: user, net-interface, expirey-signal
 	userAddr struct {
-		locked int8
+		locker sync.RWMutex
 		addr map[string][]string
 	}
 	reservedAddr struct {
-		locked int8
+		locker sync.Mutex
 		addr map[string]bool
 	}
 }
 
-func (n *Network) NewIntf () {}
+func (n *Network) NewIntf (userID, netAddr) (*Interface, error) {
+	id, errX := str.UniquePredsafeStr (32)
+	if errX != nil {
+		errMssg := fmt.Sprintf ("Unable to generate ID for interface. [%s]", errX.Error ())
+		return nil, errors.New (errMssg)
+	}
+	intf := Interface {
+		underlyingNetwork: n,
+		interfaceID: id
+		user: userID
+}
 
 func (n *Network) GetUser () {}
 
@@ -47,20 +57,72 @@ func (n *Network) GetStoreP () {}
 
 // --------------
 
+func (n *Network) CheckUsage (netAddr string) (string) {}
+
 func (n *Network) Reserve () {}
 
-func (n *Network) ScanReserver () {}
+func (n *Network) ScanReserve () {}
 
-func (n *Network) Release () {}
+func (n *Network) Release (netAddr string) (error) {
+	n.reservedAddr.Lock ()
+	delete (n.reservedAddr.addr, netAddr)
+	n.reservedAddr.Unlock ()
+}
 
 func (n *Network) Reclaim () {}
 
-func (n *Network) Lock () {}
+// -------- completed -------------
 
-func (n *Network) Unlock () {}
+func (n *Network) Lock () (*Unlocker) {
+	n.locked.Lock ()
+	n.locked.locked = true
+	return &Unlocker {locker: sync.RWMutex {}, underlyingNet: n, used: false}
+}
 
-func (n *Network) Freeze () {}
+func (n *Network) Locked () (bool) {
+	return n.locked.locked
+}
 
-func (n *Network) Freezed () {}
+type Unlocker struct {
+	locker sync.RWMutex
+	underlyingNet *Network
+	used bool
+}
 
-func (n *Network) Unfreeze () {}
+func (u *Unlocker) Unlock () {
+	u.locker.Lock ()
+	defer u.locker.Unlock ()
+	if u.used == true {
+		return
+	}
+	u.underlyingNet.locked.locked = false
+	u.underlyingNet.locked.locker.Unlock ()
+	u.used = true
+}
+
+func (n *Network) Freeze () (*Unfreezer) {
+	n.freezed.Lock ()
+	n.freezed.freezed = true
+	return &Unfreezer {locker: sync.RWMutex {}, underlyingNet: n, used: false}
+}
+
+func (n *Network) Freezed () (bool) {
+	return n.freezed.freezed
+}
+
+type Unfreezer struct {
+	locker sync.RWMutex
+	underlyingNet *Network
+	used bool
+}
+
+func (u *Unfreezer) Unfreeze () {
+	u.locker.Lock ()
+	defer u.locker.Unlock ()
+	if u.used == true {
+		return
+	}
+	u.underlyingNet.freezed.freezed = false
+	u.underlyingNet.freezed.locker.Unlock ()
+	u.used = true
+}
