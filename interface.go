59a03cbed252a9c5bba0088eccb0313479c8a6da
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-08-02 05:38:19 +0100

    Progress 13

--- interface.go
+++ interface.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"gopkg.in/qamarian-lib/str.v1"
 	"sync"
+	"sync/atmoic"
 )
 
 type Interface struct {
@@ -17,35 +18,120 @@ type Interface struct {
 
 	closed bool
 	harvestBasket list.List
-	deliveryStore store
+	deliveryStore *store
 
 	cache spCache
 }
 
+func (i *Interface) getID () (string) {
+	return i.id
+}
+
+func (i *Interface) getUNet () (*Network) {
+	return i.underlyingNet
+}
+
+func (i *Interface) getUser () (string) {
+	return i.user
+}
+
+func (i *Interface) getNetAddr () (string) {
+	return i.netAddr
+}
+
+func (i *Interface) getClosedSig () (bool) {
+	return i.closed
+}
+
+func (i *Interface) getStore () (*store) {
+	return i.deliveryStore
+}
+
 type store struct {
 	id string
-	racks list.List
+	racks struct {
+		state int32 // 0: not in use; 1: about to-be manipulated; 2: about to-be harvested
+		racks list.List
+	}
 	wakeupSignal struct {
 		waiting bool
 		signalChan sync.Cond
 	}
 }
 
-func (s *store) AddToRack () {}
-
-type storeProtected struct {
-	underlyingStore *store
-	senderAddr string
-	lastKnownStore string
-	rack list.List
+func (s *store) getID () (string) {
+	return s.id
 }
 
-func (s *storeProtected) AddToRack () {}
+func (s *store) lockRacks () {
+	for {
+		ok := atomic.CompareAndSwapInt32 (*s.racks.state, 0, 1)
+		if ok == true {
+			break
+		}
+	}
+}
+
+func (s *store) unlockRacks () {
+	s.racks.state = 0
+}
+
+func (s *store) addRack (senderRack *rack) (error) {
+	if s.racks.state == 2 {
+		return StrErrToBeHarvested
+	}
+	if s.racks.Len () == 0 {
+		s.racks.PushFront (senderRack)
+	} else {
+		s.racks.PushBack (senderRack)
+	}
+	s.racks.state = 0
+}
+
+var (
+	StrErrToBeHarvested error = errors.New ("The store is about to be harvested.")
+)
 
 type spCache struct {
 	locker sync.RWMutex
 	storeP map[string]*storeProtected
 }
+
+type storeProtected struct {
+	recipientInt *Interface
+	senderAddr string
+	lastKnownStore string
+	senderRack *list.List
+}
+
+func (s *storeProtected) addMessage (mssg interface {}) (error) {
+	if s.recipientInt.getNetAddr () == "" {
+		return StpErrNotConnected
+	} else if s.recipientInt.getClosedSig () == true {
+		return StpErrClosed
+	}
+	s.recipientInt.getStore ().lockRacks ()
+	defer s.recipientInt.getStore ().unlockRacks ()
+	if lastKnownStore != s.recipientInt.getStore ().getID () {
+		s.senderRack = &list.List {}
+		errX := s.recipientInt.getStore ().addRack (s.senderRack)
+		if errX != nil {
+			errMssg := fmt.Sprintf ("Message could not be added to the rack. [%s]",
+				errX.Error ())
+			return errors.New (errMssg)
+		}
+	}
+	if s.senderRack.Len () == 0 {
+		s.senderRack.PushFront (mssg)
+	} else {
+		s.senderRack.PushBack (mssg)
+	}
+}
+
+var (
+	StpErrNotConnected error = errors.New ("Recipient is not connected to the network.")
+	StpErrClosed error = errors.New ("Recipient is closed to new messages.")
+)
 
 // ---------- Section A ---------- //
 // Ambassadors
@@ -83,7 +169,7 @@ func (i *Interface) provideSP (netAddr string) (*storeProtected, error) {
 		return nil, IntErrNotConnected
 	}
 	sp := &storeProtected {
-		underlyingStore: &(i.deliveryStore),
+		underlyingInt: i,
 		senderAddr: netAddr,
 		lastKnownStore: "",
 		rack: list.List {},
@@ -114,7 +200,7 @@ func (i *Interface) Send () {}
 
 func (i *Interface) Read () {}
 
-func (i *Interface) Check () {}
+func (i *Interface) Wait () {}
 
 func (i *Interface) Close () {
 	i.closed = false
